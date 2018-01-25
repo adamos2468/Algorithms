78f1e1796e05e5becc6296635da8d7f8b34f3774
adamos2468 <adamos2468@gmail.com>
2018-01-25 12:16:55 +0200

    Added Breadth-first search algorithm implementation

---
+++ Graph Algorithms/Graph Traversal/BFS.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<queue>
+#define MAX_NODES 100000
+
+using namespace std;
+
+queue <int> nextNodes;
+vector< vector<int> > adjList(MAX_NODES, vector<int>()); 
+bool visited[MAX_NODES];
+
+void bfs(int s)
+{
+    visited[s]=true;
+    nextNodes.push(s);
+    while(!nextNodes.empty())
+    {
+        int nextNode=nextNodes.front();
+        nextNodes.pop();
+        for(int i=0; i<adjList[nextNode].size(); i++)
+            if(!visited[adjList[nextNode][i]])
+            {
+                visited[adjList[nextNode][i]]=true;
+                nextNodes.push(adjList[nextNode][i]);
+            }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    for(int i=0; i<m; i++)
+    {
+        int x, y;
+        cin>>x>>y;
+        adjList[x].push_back(y);
+        adjList[y].push_back(x);
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(!visited[i])
+            bfs(i);
+    }
+}
