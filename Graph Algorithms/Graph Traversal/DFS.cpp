c0da9b0155e1dcb128a2d33d91a2c2304c299692
adamos2468 <adamos2468@gmail.com>
2018-01-25 12:18:06 +0200

    Added Depth-first search algorithm implementation

---
+++ Graph Algorithms/Graph Traversal/DFS.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#define MAX_NODES 100000
+using namespace std;
+vector< vector<int> > adjList(MAX_NODES, vector<int>()); 
+bool visited[MAX_NODES];
+void dfs(int s)
+{
+    visited[s]=true;
+    for(int i=0; i<adjList[s].size(); i++)
+        if(!visited[s])
+            dfs(adjList[s][i]);
+}
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    for(int i=0; i<m; i++)
+    {
+        int x, y;
+        cin>>x>>y;
+        adjList[x].push_back(y);
+        adjList[y].push_back(x);
+    }
+    for(int i=0; i<n; i++)
+        if(!visited[i])
+            dfs(i);
+}
