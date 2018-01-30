c2761eba7d86de677332a4aa95ce87a9f0c3335f
adamos2468 <adamos2468@gmail.com>
2018-01-30 18:18:39 +0200

    Added Dijkstra O(N^2) Implementation

---
+++ Graph Algorithms/Shortest Path/SlowDijkstra.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#define MAX_NODES 100000
+#define INF 1000000007
+using namespace std;
+
+vector< vector< pair<int, int> > > adjList(MAX_NODES, vector< pair<int, int> >()); 
+int dist[MAX_NODES];
+bool visited[MAX_NODES];
+void reset(int n)
+{
+    for(int i=0; i<n; i++){
+        dist[i]=INF;
+        visited[i]=INF;
+    }
+}
+void dijkstra(int start, int nodes)
+{
+    dist[start]=0;
+    int curr=start;
+    for(int t=0; t<nodes-1; t++)
+    {
+        visited[curr]=true;
+        for(int i=0; i<adjList[curr].size(); i++)
+        {
+            dist[adjList[curr][i].first]=min(dist[adjList[curr][i].first], dist[curr]+adjList[curr][i].second);
+        }
+        int tempc=curr;
+        int temp=dist[curr];
+        dist[curr]=INF;
+        for(int i=0; i<nodes; i++)
+        {
+            if(!visited[i] && dist[i]<dist[curr])
+            {
+                curr=i;
+            }
+        }
+        dist[tempc]=temp;
+    }
+}
+int main()
+{
+    int n, m, start;
+    cin>>n>>m>>start;
+    for(int i=0; i<m; i++)
+    {
+        int x, y, w;
+        cin>>x>>y>>w;
+        adjList[x].push_back(make_pair(y, w));
+        adjList[y].push_back(make_pair(x, w));
+    }
+    dijkstra(start, n);
+    for(int i=0; i<n; i++)
+    {
+        cout<<"Shortest Path from "<<start<<" to "<<i<<" is "<<dist[i]<<endl;
+    }
+}
