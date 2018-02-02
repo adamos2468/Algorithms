2e7192e4f6a643e71a03bcf548ec13d208bebd86
adamos2468 <adamos2468@gmail.com>
2018-02-02 13:18:57 +0200

    Added Tree Traversal Implementation

---
+++ Graph Algorithms/Tree Algorithms/TreeTraversal.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#define MAX_NODES 100000
+using namespace std;
+
+vector< vector<int> > adjList(MAX_NODES, vector<int>());
+
+void trip(int curr, int prev)
+{
+    for(int i=0; i<adjList[curr].size(); i++)
+        if(adjList[curr][i]!=prev)
+            trip(adjList[curr][i], curr);
+}
+
+int main()
+{
+    int n, m;
+    for(int i=0; i<m; i++)
+    {
+        int x, y;
+        cin>>x>>y;
+        adjList[x].push_back(y);
+        adjList[y].push_back(x);
+    }
+    trip(0, -1);
+}
