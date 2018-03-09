6128d3213b3f801fea2164ec52c33f930af75770
adamos2468 <adamos2468@gmail.com>
2018-03-09 19:56:46 +0200

    Added Permutation Generator Implementations

---
+++ Programming Techinques/Complete Search/Brute-Force/GeneratingPermutation.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#define N 10
+using namespace std;
+
+vector<int> permutation;
+bool chosen[N];
+void generate(int n){
+    if(permutation.size()==n)
+    {
+        for(int i=0; i<n; i++)
+            cout<<permutation[i]<<" ";
+        cout<<endl;
+        return;
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(chosen[i])
+            continue;
+        chosen[i]=true;
+        permutation.push_back(i);
+        generate(n);
+        chosen[i]=false;
+        permutation.pop_back();
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    generate(n);
+}
---
+++ Programming Techinques/Complete Search/Brute-Force/GeneratingPermutationSTL.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> permutation;
+void generate(int n){
+    for(int i=0; i<n; i++)
+        permutation.push_back(i);
+    do{
+        for(int i=0; i<n; i++)
+            cout<<permutation[i]<<" ";
+        cout<<endl;
+    }while(next_permutation(permutation.begin(), permutation.end()));
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    generate(n);
+}
