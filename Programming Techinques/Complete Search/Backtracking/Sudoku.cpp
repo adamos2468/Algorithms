58f40b9c2e959b6ef0bad4853e8f95213d2d12d9
adamos2468 <adamos2468@gmail.com>
2018-03-12 15:07:23 +0200

    Added Implementation for Sudoku Solver

---
+++ Programming Techinques/Complete Search/Backtracking/Sudoku.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#define N 64
+using namespace std;
+
+int Sudoku[N][N];
+unsigned long long rows[N], col[N];
+
+bool solve(int r, int c, int n)
+{
+    if(Sudoku[r][c]==-1)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if((rows[r]&(1<<i))==0 && (col[c]&(1<<i))==0)
+            {
+                rows[r]|=1<<i; col[c]|=1<<i; Sudoku[r][c]=i;
+                bool ans;
+                if(c==n-1)
+                    return true;
+                else if(r==n-1)
+                    ans=solve(0, c+1, n);
+                else
+                    ans=solve(r+1, c, n);
+                if(ans==true)
+                    return true;
+                rows[r]&=~(1<<i); col[c]&=~(1<<i); Sudoku[r][c]=-1;
+            }
+        }
+    }
+    bool ans;
+    if(c==n-1)
+        return true;
+    else if(r==n-1)
+        ans=solve(0, c+1, n);
+    else
+        ans=solve(r+1, c, n);
+    return ans;   
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin>>Sudoku[i][j];
+            Sudoku[i][j]--;
+        }
+    }
+    bool acc=solve(0, 0, n);
+    if(acc)
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+                cout<<(++Sudoku[i][j])<<" ";
+            cout<<endl;
+        }
+    else
+        cout<<"No Solution\n";
+}
