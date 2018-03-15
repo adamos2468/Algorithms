122e66d206215bae986b8f5b7afd75402ceeaaaa
adamos2468 <adamos2468@gmail.com>
2018-03-15 15:34:10 +0200

    Added implementation for the scheduling problem

---
+++ Programming Techinques/Greedy/Scheduling.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#define N 100000
+using namespace std;
+
+pair<int, int> tasks[N];
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+        cin>>tasks[i].second>>tasks[i].first;
+    sort(tasks, tasks+n);
+    int d=0, ans=0;
+    for(int i=0; i<n; i++)
+    {
+        if(tasks[i].second>=d)
+        {
+            d=tasks[i].first;
+            ans++;
+        }
+    }
+    cout<<ans<<endl;    
+}
