83cfb7ec954b3dd9f675dae72279458da3414688
adamos2468 <adamos2468@gmail.com>
2018-03-17 11:35:53 +0200

    Added Implementation to the Knapsack0/1 problem

---
+++ Programming Techinques/Dynamic Programming/Top Down/Knapsack0-1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#define N 100
+#define W 10000
+#define INF 1000000007
+using namespace std;
+pair<int, int> items[N];
+int memo[N][W];
+int knapsack(int it, int wei)
+{
+    if(wei==0 || it<0)
+        return 0;
+    if(memo[it][wei]!=0)
+        return memo[it][wei];
+    if(wei>=items[it].first)
+        memo[it][wei]=max(knapsack(it-1, wei), knapsack(it-1, wei-items[it].first)+items[it].second);
+    else
+        memo[it][wei]=knapsack(it-1, wei);
+    return memo[it][wei];
+}
+int main()
+{
+    int n, w;
+    cin>>n>>w;
+    for(int i=0; i<n; i++)
+        cin>>items[i].first>>items[i].second;
+    cout<<knapsack(n-1, w)<<endl;
+}
