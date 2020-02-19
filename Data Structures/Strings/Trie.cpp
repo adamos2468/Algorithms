59480da37d3cf008db726df30479f6b21ccb6540
adamos2468 <adamos2468@gmail.com>
2020-02-19 09:56:03 +0300

    Added Trie

---
+++ Data Structures/Strings/Trie.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct node{
+    char child[26];
+    bool isWord;
+};
+
+void add(vector<node>& trie, string s){
+    int trieIdx=0;
+    for(auto x:s){
+        if(trie[trieIdx].child[x-'a']==0){
+            trie[trieIdx].child[x-'a']=trie.size();
+            trie.push_back(node());
+        }
+        trieIdx=trie[trieIdx].child[x-'a'];
+    }
+    trie[trieIdx].isWord=true;
+}
+
+bool find(vector<node>& trie, string s){
+    int trieIdx=0;
+    for(auto x:s){
+        if(trie[trieIdx].child[x-'a']==0)
+            return false;
+        trieIdx=trie[trieIdx].child[x-'a'];
+    }
+    return trie[trieIdx].isWord;
+}
+
+void remove(vector<node>& trie, string s){
+    int trieIdx=0;
+    for(auto x:s){
+        if(trie[trieIdx].child[x-'a']==0)
+            return;
+        trieIdx=trie[trieIdx].child[x-'a'];
+    }
+    trie[trieIdx].isWord=false;
+}
+
+int main(){
+    vector<node> trie;
+    trie.push_back(node());
+    add(trie, "anna");
+    add(trie, "anne");    
+    cout<<find(trie, "anne");
+    remove(trie, "anna");
+}
