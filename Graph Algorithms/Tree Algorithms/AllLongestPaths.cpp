68cfc0066801f7d8cf400d45af84daf9d5c309b1
Adamos Ttofari <adamos2468@gmail.com>
2020-04-18 19:46:01 +0300

    Fixed Bug

--- Graph Algorithms/Tree Algorithms/AllLongestPaths.cpp
+++ Graph Algorithms/Tree Algorithms/AllLongestPaths.cpp
@@ -1,76 +1,70 @@
-#include<iostream>
-#include<vector>
-#define MAX_NODES 100000
+#include<bits/stdc++.h>
+#define MAXN 200001
 using namespace std;
+vector<int> adj[MAXN];
+int height[MAXN];
+int fromFather[MAXN];
+int longest[MAXN];
+pair< int, int > best[MAXN];
+int getHeight(int node, int father){
+    height[node]=0;
+    priority_queue< pair<int, int> > localBest;
+    for(auto x:adj[node])
+        if(father!=x){
+            height[node]=max(getHeight(x, node)+1, height[node]);
+            localBest.push({-height[x], x});
+            while(localBest.size()>2)
+                localBest.pop();
+        }
+    if(localBest.size()==2){
+        pair<int, int> temp;
+        temp.second=localBest.top().second;
+        localBest.pop();
+        temp.first=localBest.top().second;
+        localBest.pop();
+        best[node]=temp;
+    }else if(localBest.size()==1){
+        pair<int, int> temp;
+        temp.first=localBest.top().second;
+        localBest.pop();
+        temp.second=0;
+        best[node]=temp;
+    }else{
+        best[node]={0, 0};
+    }
+    return height[node];
+}
 
-vector< vector<int> > adjList(MAX_NODES, vector<int>());
-pair<int, int> maxLength[2][MAX_NODES];
-int maxPath[MAX_NODES];
-pair<int, int> toChildren(int curr, int prev)
-{
-    if(adjList[curr].size()==1 && adjList[curr][0]==prev)
-    {
-        maxLength[0][curr]=make_pair(0, curr);
-        maxLength[1][curr]=make_pair(-1, -1);
-        return maxLength[0][curr];
-    }
-    maxLength[0][curr]=maxLength[1][curr]=make_pair(-1, -1);
-    for(int i=0; i<adjList[curr].size(); i++)
-    {
-        if(adjList[curr][i]!=prev)
-        {
-            pair<int, int> temp=toChildren(adjList[curr][i], curr);
-            temp.first++;
-            if(temp.first>maxLength[0][curr].first)
-            {
-                maxLength[1][curr]=maxLength[0][curr];
-                maxLength[0][curr]=make_pair(temp.first, adjList[curr][i]);
-            }
-            else if(temp.first>maxLength[1][curr].first)
-            {
-                maxLength[1][curr]=make_pair(temp.first, adjList[curr][i]);
-            }
+void getFather(int node, int father){
+    if(father==0){
+        fromFather[node]=0;
+        longest[node]=height[node];
+    }else{
+        fromFather[node]=fromFather[father]+1;
+        if(best[father].first==node){
+            fromFather[node]=max(fromFather[node], height[best[father].second]+(best[father].second!=0)+1);
+        }else{
+            fromFather[node]=max(fromFather[node], height[best[father].first]+2);
         }
+        longest[node]=max(fromFather[node], height[node]);
     }
-    return maxLength[0][curr];   
+    for(auto x:adj[node])
+        if(x!=father)
+            getFather(x, node);
 }
-void toFather(int curr, int prev)
-{
-    if(prev!=-1)
-    {
-        if(maxLength[0][prev].first+1>maxLength[0][curr].first && maxLength[0][prev].second!=curr)
-            maxLength[0][curr]=make_pair(maxLength[0][prev].first+1, prev);
-        else if(maxLength[1][prev].first+1>maxLength[0][curr].first)
-            maxLength[0][curr]=make_pair(maxLength[1][prev].first+1, prev);
-    }
-    maxPath[curr]=maxLength[0][curr].first;
-    if(adjList[curr].size()==1 && adjList[curr][0]==prev)
-        return;
-    for(int i=0; i<adjList[curr].size(); i++)
-    {
-        if(adjList[curr][i]!=prev)
-        {
-            toFather(adjList[curr][i], curr);
-        }
-    }
-    
-}
-int main()
-{
+
+int main(){
     int n;
     cin>>n;
-    for(int i=0; i<n-1; i++)
-    {
+    for(int i=0; i<n-1; i++){
         int x, y;
         cin>>x>>y;
-        adjList[x].push_back(y);
-        adjList[y].push_back(x);
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    int root=0;
-    toChildren(root, -1);
-    toFather(root, -1);
-    for(int i=0; i<n; i++)
-    {
-        cout<<"Length of Longest Path from "<<i<<" is "<<maxPath[i]<<"\n";
-    }
+    getHeight(1, 0);
+    getFather(1, 0);
+    for(int i=1; i<=n; i++)
+        cout<<longest[i]<<" ";
+    cout<<endl;
 }
